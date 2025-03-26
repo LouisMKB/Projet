fad306cf7bd5d88f0a4304ba8415f457e94b9af4
Louis-Marie KOUMBA BOUNDA <louismsup@gmail.com>
2025-03-26 11:43:01 +0100

    Premier ajout tmbd api

--- README.md
+++ README.md
@@ -2,8 +2,12 @@
 
 ## NOM Prénom :
 
+KOUMBA BOUNDA Louis-Marie
+
 ### **Pour rappel voici le contexte du projet :** 
 En tant que plateforme de streaming de contenus audiovisuels, nous souhaitons développer une solution de recommandation de films qui repose sur des données réelles et utilise une approche de filtrage collaboratif. L'objectif est d'offrir une expérience utilisateur personnalisée en proposant une liste de films adaptés à leurs goûts et préférences, tout en permettant une analyse approfondie des données collectées.
+
+---
 
 Pour ce faire le projet sera séparé en trois parties :
 
--- backend/app/utils/data_loader.py
+++ backend/app/utils/data_loader.py
@@ -1,6 +1,12 @@
 import pandas as pd
 
 def load_movie_metadata():
-    # Charger les métadonnées des films (exemple avec ml-100k)
-    pass
     #A faire
+    def load_a_modifier():
+    url = "https://api.themoviedb.org/3/authentication"
+    headers = {
+    "accept": "application/json",
+    "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlNjEzZjcwYTM5NTZlNTQ5NThiODhlNjM1MjY2NzM0MSIsIm5iZiI6MTc0Mjk4NDAyOC4wNDcwMDAyLCJzdWIiOiI2N2UzZDM1YzQ0MGYzMTFhY2U3NjI2MjAiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.1ElkJXIgfMwtTG3QrkYLGHacvInVXrXkkjuv7cJ05mY"
+    }
+    response = requests.get(url, headers=headers)
+    print(response.text)
