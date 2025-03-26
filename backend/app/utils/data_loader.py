84cd3a2838e32c0bf6863299f5f4daf595d009ec
Louis-Marie KOUMBA BOUNDA <louismsup@gmail.com>
2025-03-26 12:16:27 +0100

    data_loader.py v2

--- backend/app/utils/data_loader.py
+++ backend/app/utils/data_loader.py
@@ -1,12 +1,14 @@
 import pandas as pd
-
+import requests
 def load_movie_metadata():
     #A faire
-    def load_a_modifier():
-    url = "https://api.themoviedb.org/3/authentication"
+
+    url = "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1"
+
     headers = {
     "accept": "application/json",
     "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlNjEzZjcwYTM5NTZlNTQ5NThiODhlNjM1MjY2NzM0MSIsIm5iZiI6MTc0Mjk4NDAyOC4wNDcwMDAyLCJzdWIiOiI2N2UzZDM1YzQ0MGYzMTFhY2U3NjI2MjAiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.1ElkJXIgfMwtTG3QrkYLGHacvInVXrXkkjuv7cJ05mY"
-    }
+    }  
     response = requests.get(url, headers=headers)
     print(response.text)
+    
